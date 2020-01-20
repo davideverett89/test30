cf3f9b2d6b288754d2c7436b4cf59367cb3075f8
David Everett <davideverett1989@gmail.com>
2020-01-19 22:54:47 -0600

    Did some JS practice.

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 console.log("Test 30");
+
+// Factory Function
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log("draw");
+        }
+    }
+}
+
+//Constructor Function
+
+const circle = createCircle(1);
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log("draw");
+    }
+}
+
+const another = new Circle(7);
+console.log(circle);
+console.log(another);
+
+let x = { value: 10};
+let y = x;
+
+x.value = 20;
+
+console.log(y);
+console.log(x);
+
+let number = 10;
+function increase(number) {
+    number++;
+}
+
+increase(number);
+console.log(number);
